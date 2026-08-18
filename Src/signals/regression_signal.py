a700af934838638c3af48bb16865d9dd259b63f2
Kurilbalkrishna <174647990+kurilbalkrishna@users.noreply.github.com>
2026-08-18 12:59:34 +0530

    Add volatility factor calculation

--- Src/signals/regression_signal.py
+++ Src/signals/regression_signal.py
@@ -11,3 +11,6 @@ print(df[["Date", "Close", "Return"]].head(10))
 
 df["Momentum_20"] = df["Close"].pct_change(periods=20)
 print(df[["Date", "Close", "Momentum_20"]].tail(10))
+
+df["Volatility_20"] = df["Return"].rolling(window=20).std()
+print(df[["Date", "Close", "Momentum_20", "Volatility_20"]].tail(10))
